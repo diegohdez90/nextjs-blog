4513b2ab4ae5c6677bc55577d5946bd7cb2b0e65
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2018-02-23 13:57:46 -0600

    feat(index):
    add Header to render in the index page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,6 @@
-const Index = ({title= 'Welcome to my blog made bt next.js'}) => <div><h2>{title}</h2></div>
+import Header from '../components/header';
+import withMui from '../shared/MUI/withMUI'
 
-export default Index
+const Index = ({title= 'Welcome to my blog made bt next.js'}) => <div><Header /><h2>{title}</h2></div>
+
+export default withMui(Index)
