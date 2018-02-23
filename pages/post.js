9b09f328c1b99a79002c1b96a2aa43b393cd53bd
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2018-02-23 17:04:25 -0600

    feat(post):
    add post content. get them through fetch isomorphic library to render content into component

--- pages/post.js
+++ pages/post.js
@@ -1,15 +1,37 @@
 import Header from '../components/header';
 import withMui from '../shared/MUI/withMUI.js'
+import { Card, CardHeader, CardText } from 'material-ui/Card'
+import { RaisedButton } from 'material-ui'
+import Link from 'next/link'
+import 'isomorphic-fetch';
 
-const Post = ({title = 'This will be the post rendered'}) =>
+const Post = ({title, content}) => 
   <div>
     <Header />
-    <h2>{title}</h2>
+    <Card>
+      <CardHeader title={title} />
+      <CardText>
+        <div dangerouslySetInnerHTML={{ __html: content }} />
+        <RaisedButton fullWidth={true}>
+          <Link href="/" as="/blog" >
+            <a>
+            Go back to blog!
+            </a>
+          </Link>
+        </RaisedButton>
+      ></CardText>
+    </Card>
   </div>
 
-Post.getInitialProps = async () => {
-
-}
-
+Post.getInitialProps = async ({ query : {id}}) => {
+  const response = await fetch(`${process.env.BLOGGER_URL}/${id}?key=${process.env.API_KEY}`)
+  const data = await response.json()
+  const {title} = data
+  const {content} = data
+  return {
+    title,
+    content
+  }
+};
 
 export default withMui(Post)
