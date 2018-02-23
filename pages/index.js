17c54b8085e7efe38ab4935de5047f649afeacad
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2018-02-23 15:12:28 -0600

    feat(index):
    add isomorphic-fech library to get post from blogger by id and a key to
    auth to get posts data and import material ui to render card header and
    buttons

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,29 @@
 import Header from '../components/header';
 import withMui from '../shared/MUI/withMUI'
+import 'isomorphic-fetch';
+import {Card, CardMedia, CardText, CardHeader} from 'material-ui/Card'
+import { RaisedButton } from 'material-ui'
 
-const Index = ({title= 'Welcome to my blog made bt next.js'}) => <div><Header /><h2>{title}</h2></div>
+const Index = ({posts}) => 
+  <div>
+    <Header />{
+      posts.map(x => 
+        <Card key={x.id} >
+          <CardHeader title={x.title} />
+          <CardText>
+            <RaisedButton label="Click to view post!" fullWidth={true} primary={true} />
+          </CardText>
+        </Card>
+    )
+    }
+  </div>
+
+Index.getInitialProps = async () => {
+  const response = await fetch(`${process.env.BLOGGER_URL}?key=${process.env.API_KEY}`)
+  const data = await response.json()
+  return {
+    posts: data.items
+  }
+}
 
 export default withMui(Index)
