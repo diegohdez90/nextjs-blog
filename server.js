39043324236bed2fd955a542f73f984da2568b54
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2018-02-23 16:35:27 -0600

    chore(server):
    add route to render blog post in post page

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ app.prepare().then(() => {
   const server = express()
   server.get('/blog', (req, res) => app.render(req, res, '/'))
   server.get('/', (req, res) => res.redirect(301, '/blog'))
+
+  server.get('/blog/:id', (req, res) => {
+    return app.render(req, res, '/post', Object.assign({id: req.params.id}, req.query))
+  })
+
+  server.get('/post', (req, res) => {
+    if (req.query.id) return res.redirect(`/blog/${req.query.id}`);
+    res.redirect(301, '/blog')
+  })
+
   server.get('/*', (req, res) => handle(req, res))
 
   server.listen(port, err => {
