278748897782c289904202f40c9292e62917163c
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2018-02-23 13:55:12 -0600

    chore(shared):
    add styles for app

---
+++ shared/MUI/theme.js
@@ -0,0 +1,7 @@
+export const PRIMARY_COLOR = '#01579b'
+export const PRIMARY_COLOR_TWO = '#0288d1'
+export const PRIMARY_COLOR_THREE = '#0091ea'
+export const ACCENT_COLOR_ONE = '#80d8ff'
+export const ACCENT_COLOR_TWO = '#b3e5fc'
+export const ACCENT_COLOR_THREE = '#e1f5fe'
+
